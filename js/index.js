b3ef4a135e4352301d76ca0b675446e05594c38b
Paul Tsai <tahsdj@gmail.com>
2017-09-05 03:55:59 +0800

    prototype completed

---
+++ js/index.js
@@ -0,0 +1,59 @@
+
+const travelList = [
+		{
+			title: '成大',
+			duration: '6:00~17:00',
+			timeLong: '1 hour',
+			imgUrl: 'img/ncku.png',
+			class: 'short'
+		},
+		{
+			title: '十鼓糖廠',
+			duration: '10:00~21:00',
+			timeLong: '2.5 hours',
+			imgUrl: 'img/sugar.png',
+			class: 'long'
+		},
+		{
+			title: '手作課程',
+			duration: '10:00~12:00',
+			timeLong: '2 hours',
+			imgUrl: 'img/experience.png',
+			class: 'short'
+		},
+		{
+			title: '虎山森林步道',
+			duration: '15:30~17:30',
+			timeLong: '1.5 hours',
+			imgUrl: 'img/tigerMountain.png',
+			class: 'short'
+		},
+		{
+			title: '奇美博物館',
+			duration: '9:00~17:00',
+			timeLong: '2 hours',
+			imgUrl: 'img/musium.png',
+			class: 'long'
+		}
+	]
+
+
+new Vue({
+	el: '#app',
+	data: {
+		travelPlan: travelList,
+		btnColor: {
+			backgroundColor: 'white'
+		},
+		btnContent: 'GO'
+	},
+	methods: {
+		btnClicked(){
+			this.btnColor = {
+				backgroundColor: '#88fbd4',
+				color: 'white'
+			}
+			this.btnContent = 'booked'
+		}
+	}
+})
